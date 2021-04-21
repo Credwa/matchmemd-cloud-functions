40c6e138754258ea49512b11b1de45580753738d
Craig Edwards <craigroe7@gmail.com>
2021-04-21 18:28:03 +0000

    set CORS to put

--- matchmemd-contacts/matchmemd-contacts.go
+++ matchmemd-contacts/matchmemd-contacts.go
@@ -91,7 +91,7 @@ func ContactRequest(w http.ResponseWriter, r *http.Request) {
 	// Set CORS headers for the preflight request
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Methods", "PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
@@ -99,7 +99,7 @@ func ContactRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "PATCH, POST")
+	w.Header().Set("Access-Control-Allow-Methods", "PATCH, PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 
 	app, firebaseErr := firebase.NewApp(context.Background(), nil)
