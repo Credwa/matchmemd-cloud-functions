33ec46172e83d633559f7f00822efe4b7cba5836
Craig Edwards <craigroe7@gmail.com>
2021-04-21 18:35:06 +0000

    missing header on request

--- matchmemd-contacts/matchmemd-contacts.go
+++ matchmemd-contacts/matchmemd-contacts.go
@@ -92,14 +92,14 @@ func ContactRequest(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "PATCH, PUT")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 
 	app, firebaseErr := firebase.NewApp(context.Background(), nil)
